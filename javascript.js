30799bd8b3595775d7c4f8dd74a84eea49763123
guyfoz77 <125807123+guyfoz77@users.noreply.github.com>
2023-03-25 18:49:12 +0000

    Write getComputerSelection function

--- javascript.js
+++ javascript.js
@@ -1,4 +1,12 @@
 //get player choice
-//computer makes random choice
+//computer makes random choice (done)
 //compare computer and placer choice
 //decide winner
+const choice = ["rock","paper","scissors"]
+
+function getComputerSelection() {
+    let rand = Math.floor(Math.random() * 3);    //rolls a random number between 0 and 2
+    return choice[rand];    //returns the item at location (rand) from the array.
+}
+
+console.log(getComputerSelection());
