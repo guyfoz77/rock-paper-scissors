d139b5d5f7ee87c46c4f48113e32ca1a6f1f3ef3
guyfoz77 <125807123+guyfoz77@users.noreply.github.com>
2023-03-25 19:01:50 +0000

    Write getPlayerChoice function

--- javascript.js
+++ javascript.js
@@ -1,12 +1,20 @@
-//get player choice
+//get player choice (done)
 //computer makes random choice (done)
 //compare computer and placer choice
 //decide winner
-const choice = ["rock","paper","scissors"]
 
-function getComputerSelection() {
+//Validate user input?
+
+const choice = ["rock","paper","scissors"]
+let playerChoice
+
+function getPlayerChoice() {
+    return selection = (prompt("Rock, paper or scissors?")).toLowerCase(); //obtains a selection from the user and converts it to lower case.
+}
+
+function getComputerChoice() {
     let rand = Math.floor(Math.random() * 3);    //rolls a random number between 0 and 2
     return choice[rand];    //returns the item at location (rand) from the array.
 }
 
-console.log(getComputerSelection());
+console.log(getPlayerChoice());
