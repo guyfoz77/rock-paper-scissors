3125e689f580800f4110a66f2eb3a2b3f509747d
guyfoz77 <125807123+guyfoz77@users.noreply.github.com>
2023-03-26 13:10:24 +0100

    Complete the checkVictory function

--- javascript.js
+++ javascript.js
@@ -5,8 +5,8 @@
 
 //Validate user input?
 
-const choice = ["rock","paper","scissors"]
-let playerChoice
+const choice = ["rock","paper","scissors"];
+let victory;
 
 function getPlayerChoice() {
     return selection = (prompt("Rock, paper or scissors?")).toLowerCase(); //obtains a selection from the user and converts it to lower case.
@@ -17,4 +17,39 @@ function getComputerChoice() {
     return choice[rand];    //returns the item at location (rand) from the array.
 }
 
-console.log(getPlayerChoice());
+function checkVictory(playerChoice, computerChoice) {
+    if (playerChoice == computerChoice) {   //checks for a draw
+        victory = "draw"
+    } else if (computerChoice == "rock") {      //these else if statements check each computerChoice against player choice and...
+        switch(playerChoice) {                      // ...determines a victor.
+            case "paper": victory = "win";
+            break;
+            case "scissors": victory = "lose";
+            break;
+            default: victory = "invalidSelection";      //these default selections fire if an invalid input is passed by the user.
+        }   
+    } else if (computerChoice == "paper") {
+        switch(playerChoice) {
+            case "scissors": victory = "win";
+            break;
+            case "rock": victory = "lose";
+            break;
+            default: victory = "invalidSelection";
+        } 
+    } else if (computerChoice == "scissors") {
+        switch(playerChoice) {
+            case "rock": victory = "win";
+            break;
+            case "paper": victory = "lose";
+            break;
+            default: victory = "invalidSelection";
+        } 
+    }
+    return victory;
+}
+
+let computerChoice = getComputerChoice();
+let playerChoice = getPlayerChoice();
+console.log(`computer ${computerChoice}`);
+console.log(`player ${playerChoice}`);
+console.log(checkVictory(playerChoice, computerChoice));
